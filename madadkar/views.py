e751e6759c4c0a536bd1de3f16c1bc707149a630
niloofar <niloofar.zrf@gmail.com>
2018-02-04 19:50:47 +0330

    Madadju register

--- madadkar/views.py
+++ madadkar/views.py
@@ -4,7 +4,15 @@ from MySite.forms import ContactForm
 from django.contrib.auth import logout as auth_logout
 from django.http import HttpResponseRedirect
 from django.urls import reverse
-
+from madadju.forms import ReportForm
+from madadju.models import Madadju
+from karbar.models import MyUser
+from django.contrib.auth.models import User
+import datetime
+from karbar.forms import SignupForm2, SignupForm1
+from madadkar.models import Madadkar
+from django.contrib.auth import authenticate, login as auth_login
+from django.views import View
 
 def madadkarhome(request):
     return render(request, "madadkar/home.html")
@@ -75,10 +83,38 @@ def managesaving(request):
 def receipt(request):
     return render(request, "madadkar/receipt.html")
 
+class Report(TemplateView):
+    template_name = 'madadkar/report.html'
 
-def report(request):
-    return render(request, "madadkar/report.html")
+    def get(self, request, **kwargs):
+        form = ReportForm()
+        return render(request, self.template_name, {'form': form})
 
+    def post(self, request):
+        form = ReportForm(request.POST)
+        text = None
+        id = request.POST.get('text')
+        user=User.objects.get(username=id)
+        user=MyUser.objects.get(user=user)
+        madadju=Madadju.objects.get(user=user)
+        date = datetime.datetime.now()
+        if(madadju!=None):
+            title=request.POST.get('title')
+            content=request.POST.get('content')
+            report=Report.objects.create(madadju=madadju, content=content, title=title, date=date)
+        if form.is_valid():
+            # post = form.save(commit=False)
+            # post.user = request.user
+            # post.save()
+            form.save()
+            text = form.cleaned_data
+            form = ContactForm()
+        context={}
+        message = "گزارش شما با موفقیت ثبت شد"
+        context['message'] = message
+        context['type'] = 'green'
+        args = {'form': form, 'text': text}
+        return render(request, 'madadju/madadju.html', context)
 
 def seemsg(request):
     return render(request, "madadkar/seemsg.html")
@@ -99,5 +135,76 @@ def logout(request):
     auth_logout(request)
     return HttpResponseRedirect(reverse('home'))
 
-def ProfileView(request, username):
-    return render(request, 'madadkar/profile.html')
+
+def madadkarprofile(request):
+    user = request.user
+    user_form = SignupForm2(instance=user)
+    myUser = MyUser.objects.get(user=request.user)
+    madadkar = Madadkar.objects.get(user=myUser)
+    if request.user.is_authenticated:
+        if request.method == 'POST':
+            user_form = SignupForm2(request.POST, instance=request.user)
+            # myUser = MyUser.objects.get(user=request.user)
+            if user_form.is_valid():
+                print("valid")
+                user_form.save()
+                myUser.user = request.user
+                myUser.phone_number = request.POST.get('phone_number')
+                myUser.national_id = request.POST.get('national_id')
+                myUser.country = request.POST.get('country')
+                myUser.city = request.POST.get('city')
+                myUser.address = request.POST.get('address')
+                myUser.postal_code = request.POST.get('postal_code')
+                madadkar.employment_date = request.POST.get('employment_date')
+                myUser.save()
+                madadkar.save()
+                return render(request, 'madadkar/home.html', {'message':"تغییرات با موفقیت ثبت شد"})
+            else:
+                print(user_form.errors)
+    return render(request, 'madadkar/profile.html', {'user': user, 'myUser': myUser, 'madadkar':madadkar})
+
+
+
+class MadadkarMadadjuregister(View):
+    login_required = True
+    template_name = 'madadkar/madadju-register.html'
+
+    def get(self, request, **kwargs):
+        form = SignupForm1()
+        return render(request, self.template_name, {'form': form})
+
+    def post(self, request):
+        form = SignupForm1(request.POST)
+        phone_number = request.POST.get('phone_number')
+        national_id = request.POST.get('national_id')
+        country = request.POST.get('country')
+        city = request.POST.get('city')
+        address = request.POST.get('address')
+        postal_code = request.POST.get('postal_code')
+        physical_state = request.POST.get('physical_state')
+        age = request.POST.get('age')
+        grade = request.POST.get('grade')
+        gender = request.POST.get('gender')
+        context = {'phone_number': phone_number, 'country': country, 'city': city,
+                   'postal_code': postal_code, 'address': address, national_id: 'national_id', 'physical_state':physical_state, 'age':age,
+                    'grade':grade, 'gender':gender}
+        if form.is_valid():
+            if len(phone_number) == 11 and phone_number[0:2] == '09':
+                form.save()
+                username = form.cleaned_data.get('username')
+                raw_password = form.cleaned_data.get('password1')
+                user = authenticate(username=username, password=raw_password)
+                auth_login(request, user)
+                member = MyUser.objects.create(user=user, phone_number=phone_number, country=country, city=city,
+                                               postal_code=postal_code, address=address, national_id=national_id)
+                member.save()
+                madadkar = Madadkar.objects.create(user=member, gender=gender, grade=grade, age=age, physical_state=physical_state)
+                madadkar.save()
+                return HttpResponseRedirect(reverse('madadju-home'))
+            else:
+                phone_number_error = "شماره تلفن باید 11 رقمی باشد و با 09 آغاز شود."
+                context['phone_number_error'] = phone_number_error
+        context['form'] = form
+        context['type'] = 'signup'
+        return render(request, 'madadkar/madadju-register.html', context)
+
