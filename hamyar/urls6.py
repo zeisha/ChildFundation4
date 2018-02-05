518f1df547b5d7d405b4c0aab8963b14a851feba
zahra shabani <ze.shabani@gmail.com>
2018-02-05 17:06:34 +0330

    hamyarcheck2

--- hamyar/urls6.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import *
-
-urlpatterns = [
-    path('', Enserafview2, name = 'test'),
-    path('<int:pk>/', message_detail, name='hamyar-message-detail'),
-]
